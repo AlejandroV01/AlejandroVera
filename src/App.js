2662c71c8aae1ca08614a2fced6b42a687c0b68f
AlejandroV01 <alexvera0109@gmail.com>
2022-09-14 17:18:28 -0700

    SASS Delete, Additions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/navbar";
+import ProjectSection from "./components/ProjectSection/Project";
+import Section1 from "./components/Section1/section1";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Section1 />
+      <ProjectSection />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
